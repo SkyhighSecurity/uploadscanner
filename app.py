dd89be3e0fe8fb9a4b980f147bc3580069a426d4
Nate Brady <nate@ultimus.cloud>
2023-06-29 10:36:48 -0500

    initial push

---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+import random
+import requests
+from flask import Flask, request, redirect, flash, get_flashed_messages
+import boto3
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Maximum file size: 16MB
+app.secret_key = os.getenv('SECRET_KEY')
+
+s3 = boto3.client('s3', 
+                  aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                  region_name=os.getenv('AWS_REGION'),
+                  endpoint_url=os.getenv('AWS_S3_ENDPOINT_URL'))
+
+bucket_name = 'your-s3-bucket'  # Replace with your S3 bucket name
+
+SCAN_API_BASE_URL = os.getenv('SCAN_API_BASE_URL')
+
+def scanfile(file):
+    random_number = str(random.randint(1000000, 9999999))
+    url = f'{SCAN_API_BASE_URL}/GAMScanServer/v1/scans/cScanner/{random_number}'
+
+    file.seek(0)  # reset file pointer to the beginning
+    files = {'file': file}
+    response = requests.post(url, files=files)
+
+    if response.status_code != 200:
+        flash('File scanning failed due to server error.')
+        return True  # Consider server error as scan failure
+    
+    json_response = response.json()
+
+    if json_response.get('MalwareName'):
+        flash('File is infected and cannot be accepted.')
+        return True
+    else:
+        return False
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if file and scanfile(file) == False:
+            filename = secure_filename(file.filename)
+            s3.upload_fileobj(file, bucket_name, filename)
+            flash('File successfully uploaded and stored.')
+            return redirect(request.url)
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    {}  <!-- Display the flash message here -->
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''.format('<br>'.join([f'<p style="color: red;">{message}</p>' for message in get_flashed_messages()]))
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
