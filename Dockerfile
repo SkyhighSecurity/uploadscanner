e9972ad5109fdfd6c81744af7ce743e022933abc
Nate Brady <nate@ultimus.cloud>
2023-06-29 12:37:56 -0500

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.9-slim-buster
-
+LABEL org.opencontainers.image.source="https://github.com/SkyhighSecurity/uploadscanner"
 WORKDIR /app
 COPY . /app
 RUN pip install --no-cache-dir -r requirements.txt
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 import random
 import requests
+import base64
+import json
 from flask import Flask, request, redirect, flash, get_flashed_messages
 import boto3
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Maximum file size: 16MB
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 * 1024 # Maximum file size: 16GB
 app.secret_key = os.getenv('SECRET_KEY')
 
 s3 = boto3.client('s3', 
@@ -18,13 +20,33 @@ s3 = boto3.client('s3',
 bucket_name = os.getenv('S3_BUCKET_NAME')  # Replace with your S3 bucket name
 
 def scanfile(file):
-    random_number = str(random.randint(1000000, 9999999))
     SCAN_API_BASE_URL = os.getenv('SCAN_API_BASE_URL')
-    url = f'{SCAN_API_BASE_URL}/GAMScanServer/v1/scans/cScanner/s{random_number}'
+    SCAN_API_USERNAME = os.getenv('SCAN_API_USERNAME')
+    SCAN_API_PASSWORD = os.getenv('SCAN_API_PASSWORD')
+    
+    random_number = str(random.randint(1000000, 9999999))
+    url = f'{SCAN_API_BASE_URL}/GAMScanServer/v1/scans/cScanner/{random_number}'
 
     file.seek(0)  # reset file pointer to the beginning
-    files = {'file': file}
-    response = requests.post(url, files=files)
+    file_content = file.read()
+    file_content_base64 = base64.b64encode(file_content).decode('utf-8')  # Encode to BASE64 and decode to string
+
+    payload = {
+        'Body': file_content_base64,
+        'SourceURL': 'aHR0cDovLzAvZmlsZS9hYmMxMjMuZGF0',
+    }
+    
+    headers = {'Content-Type': 'application/json'}
+
+    # Get the basic auth username and password from environment variables
+
+
+    response = requests.post(url, 
+                             data=json.dumps(payload), 
+                             headers=headers, 
+                             auth=(SCAN_API_USERNAME, SCAN_API_PASSWORD))
+
+
 
     if response.status_code != 200:
         flash('File scanning failed due to server error.')
@@ -53,6 +75,7 @@ def upload_file():
         
         if file and scanfile(file) == False:
             filename = secure_filename(file.filename)
+            file.seek(0)
             s3.upload_fileobj(file, bucket_name, filename)
             flash('File successfully uploaded and stored.')
             return redirect(request.url)
